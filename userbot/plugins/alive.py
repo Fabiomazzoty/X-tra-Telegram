0254d74237bc07789a604583b93c817c223bfc2e
Fabiomazzoty <65749057+Fabiomazzoty@users.noreply.github.com>
2020-07-23 11:50:14 -0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,12 +6,12 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Guide.](https://how2techy.com/xtra-guide1/)"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**Bot do Fabio.** [Check Guide.](https://how2techy.com/xtra-guide1/)"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Currently Alive, my peru master!` **ψ(｀∇´)ψ**\n\n"
+    await alive.edit("`Está funcionando perfeitamente, mestre !` **ψ(｀∇´)ψ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
                      "`Bot created by:` [SnapDragon](tg://user?id=719877937), @anubisxx\n"
